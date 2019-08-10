3d723d5661a95533ce13d3d621724c0948ae325d
Alvaro <alvarocarvalho@live.com>
2019-08-10 17:37:54 -0300

    +Form de cadastro

--- blog-app/src/app/pages/register/register.page.ts
+++ blog-app/src/app/pages/register/register.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+//import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
@@ -7,9 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  cadastroForm: FormGroup;
+
+  constructor( public formbuilder: FormBuilder, public router: Router ) {
+
+    this.cadastroForm = this.formbuilder.group({
+  		nome: ['', [Validators.required, Validators.minLength(3)]],
+  		email: ['', [Validators.required, Validators.email]],
+  		senha: ['', [Validators.required, Validators.minLength(6)]],
+      senhaConfirmacao: ['', [Validators.required, Validators.minLength(6)]],
+  	});
+  }
 
   ngOnInit() {
   }
 
+  cadastrarUsuario( cadastroForm: FormGroup) {
+    console.log(cadastroForm.value);
+  }
 }
