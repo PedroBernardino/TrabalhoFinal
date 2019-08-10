3d498e5480bb265965cd6855efd9fce4658d8876
tanaka <tanaka012@outlook.com>
2019-08-10 20:07:33 -0300

    + card-home

--- blog-app/src/app/components/posts-cards/posts-cards.component.ts
+++ blog-app/src/app/components/posts-cards/posts-cards.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsCardsComponent implements OnInit {
 
+  name = 'User';
+  post_image = 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y';
+  avatar_image = 'https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y';
+
+  post_title = 'Titulo super interessante';
+
   constructor() { }
 
   ngOnInit() {}
--- blog-app/src/app/pages/home/home.page.ts
+++ blog-app/src/app/pages/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PostsService } from '../../service/posts.service';
 
 @Component({
   selector: 'app-home',
